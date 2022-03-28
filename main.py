7c56586396ac8a67e0d7b1705365573db9adef2d
Fred <18fjc1@queensu.ca>
2022-03-28 15:07:20 -0400

    Implemented clicking functionality

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ yAvg = 0
 
 
 while True:
+    font = cv2.FONT_HERSHEY_SIMPLEX
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
@@ -29,6 +30,7 @@ while True:
     yAvg = 0
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            lmList = []
             for id, lm in enumerate(handLms.landmark):
                 #print(id,lm)
                 h, w, c = img.shape
@@ -37,11 +39,46 @@ while True:
                 cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
                 xAvg = xAvg + cx
                 yAvg = yAvg + cy
+                lmList.append([id, cx, cy])
 
+            #CURSOR CODE STARTS HERE
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
             xAvg = xAvg / 20
             yAvg = yAvg / 20
-            pyautogui.moveTo(-3*cx, 3*cy)
+            pyautogui.moveTo(3*cx, 3*cy)
+
+            #CLICKING CODE STARTS HERE
+            indexX = 0
+            indexY = 0
+            indexMid = 0
+            handBottomX = 0
+            handBottomY = 0
+            pinkyX = 0
+            pinkyY = 0
+            fistWarning = "Fist!"
+            for lms in lmList:
+                if lms[0] == 7:
+                    indexX, indexY = lms[1], lms[2]
+                    # cv2.circle(handsFrame, (lms[1], lms[2]), 15, (255, 0, 255), cv2.FILLED)
+                elif lms[0] == 5:
+                    indexMid = lms[2]
+                # elif lms[0] == 11:
+                # middleY = lms[2]
+                # cv2.circle(handsFrame, (lms[1], lms[2]), 15, (255, 0, 255), cv2.FILLED)
+                # elif lms[0] == 15:
+                # ringY = lms[2]
+                # cv2.circle(handsFrame, (lms[1], lms[2]), 15, (255, 0, 255), cv2.FILLED)
+                elif lms[0] == 19:
+                    pinkyX, pinkyY = lms[1], lms[2]
+                    # cv2.circle(handsFrame, (lms[1], lms[2]), 15, (255, 0, 255), cv2.FILLED)
+                elif lms[0] == 0:
+                    handBottomX, handBottomY = lms[1], lms[2]
+            if (indexY < handBottomY) and (indexY > indexMid):
+                cv2.rectangle(imgRGB, (indexX, indexY), (pinkyX, handBottomY), (0, 0, 255), 2)
+                cv2.putText(imgRGB, fistWarning, (pinkyX + 2, indexY - 2), (font), .7,
+                            (0, 0, 255), 1, cv2.LINE_4)
+                print("Fist!!")
+                pyautogui.click()
             #if xAvg < 160 and yAvg < 120:
             #    pyautogui.press('w')
             #elif xAvg < 160 and yAvg > 120:
@@ -56,7 +93,7 @@ while True:
     fps = 1/(cTime-pTime)
     pTime = cTime
 
-    image = cv2.flip(img, 1)
+    #image = cv2.flip(img, 1)
 
     #cv2.putText(image,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
 
@@ -73,5 +110,5 @@ while True:
 
 
 
-    cv2.imshow("Image", image)
+    cv2.imshow("Image", img)
     cv2.waitKey(1)
